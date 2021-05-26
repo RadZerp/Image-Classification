d551103fe8516a69242b0aaf30c2d1361411873c
Sindre Eiklid <sindre0830@gmail.com>
2021-05-26 10:47:02 +0200

    Restructured output log during dataset init

--- datasetParser.py
+++ datasetParser.py
@@ -4,16 +4,13 @@ import requests
 
 def initilizeDataset():
     if (path.exists("./dataset") == False):
-        print("Didn't find dataset...")
         print("Requesting compressed data, this might take a while...")
         url = 'http://www.josiahwang.com/dataset/leedsbutterfly/leedsbutterfly_dataset_v1.0.zip'
         r = requests.get(url, allow_redirects=True)
         open('compressedData.zip', 'wb').write(r.content)
-        print("Uncompresses data...")
+        print("Uncompressing data...")
         with zipfile.ZipFile("compressedData.zip", 'r') as zip_ref:
             zip_ref.extractall("./dataset")
-        print("Removing compressed data...")
+        print("Deleting temporary files...")
         remove('compressedData.zip')
-        print("Dataset is initializied!")
-    else:
-        print("Found dataset!")
+    print("Dataset is initializied")
