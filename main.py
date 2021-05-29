4a8b69d22f8cc453be1ca6bbc83d40f8d7948ce8
Sindre Eiklid <sindre0830@gmail.com>
2021-05-29 15:48:50 +0200

    Tuned model for RGB images and reformatted main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import KFold, cross_val_score
+import logging
+#remove warnings outputted by tensorflow during cross validation
+tf.get_logger().setLevel(logging.ERROR)
 
-#get dataset
+print("\n\nGet dataset:\n\n")
 
 if datasetIsCached():
     data, labels = loadCachedDataset()
@@ -17,49 +20,48 @@ else:
     data = segmentData(images, masks)
     cacheDataset(data, labels)
 
-#define values
+print("\n\nDefine model:\n\n")
 
 iterations = 50
-batchSize = 256
+batchSize = 512
 model = defineModel()
 model.summary()
 
-#validate dataset
+print("\n\nValidate dataset:\n\n")
 
-# crossVal_model = tf.keras.wrappers.scikit_learn.KerasClassifier(
-#     build_fn = defineModel, 
-#     epochs = iterations, 
-#     batch_size = batchSize, 
-#     verbose = 0
-# )
+print("Running cross validation...")
+crossVal_model = tf.keras.wrappers.scikit_learn.KerasClassifier(
+    build_fn = defineModel, 
+    epochs = iterations, 
+    batch_size = batchSize, 
+    verbose = 0
+)
+kfold = KFold(n_splits = 5, shuffle = True, random_state = 0) 
+results = cross_val_score(crossVal_model, data, labels, cv = kfold)
+print("Cross validation results: " + str(results))
+print("%0.2f accuracy with a standard deviation of %0.2f" % (results.mean(), results.std()))
 
-# print("Running cross validation...")
-# kfold = KFold(n_splits = 5, shuffle = True, random_state = 0) 
-# results = cross_val_score(crossVal_model, data, labels, cv = kfold)
-# print("Cross validation results: " + str(results))
-# print("%0.2f accuracy with a standard deviation of %0.2f" % (results.mean(), results.std()))
+print("\n\nTrain model:\n\n")
 
-#train model
-
-X_train, X_test, y_train, y_test = prepareData(data, labels)
+xTrain, xTest, yTrain, yTest = prepareData(data, labels)
 results = model.fit(
-    X_train, 
-    y_train, 
-    validation_data = (X_test, y_test), 
+    xTrain, 
+    yTrain, 
+    validation_data = (xTest, yTest), 
     batch_size = batchSize, 
     epochs = iterations,
-    verbose = 1
+    verbose = 0
 )
-model.evaluate(X_test, y_test)
+model.evaluate(xTest, yTest)
 plot(results)
 
-#validate model
+print("\n\nValidate model:\n\n")
 
-Y_pred = model.predict(X_test)
-Y_pred = np.argmax(Y_pred, axis = 1)
-y_test = np.argmax(y_test, axis = 1)
+yPred = model.predict(xTest)
+yPred = np.argmax(yPred, axis = 1)
+yTest = np.argmax(yTest, axis = 1)
 target_names = ["1","2","3","4","5","6","7","8","9","10"]
-print(confusion_matrix(y_test, Y_pred))
-print(classification_report(y_test, Y_pred, target_names = target_names, zero_division = 1))
+print(classification_report(yTest, yPred, target_names = target_names, zero_division = 1))
+print(confusion_matrix(yTest, yPred))
 
 #tf.keras.utils.plot_model(model, show_shapes = True)
--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from keras.layers.pooling import *
 from keras.layers.core import *
 
 def prepareData(data, labels):
-    X_train, X_test, y_train, y_test = train_test_split(data, labels, train_size = 0.70)
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, train_size = 0.80, random_state = 0)
     y_train = np_utils.to_categorical(y_train, num_classes = 10)
     y_test = np_utils.to_categorical(y_test, num_classes = 10)
     return X_train, X_test, y_train, y_test
@@ -17,34 +17,21 @@ def defineModel():
     model = Sequential([
         #https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
         
-        Conv2D(44, (3, 3), input_shape = (100, 100, 3), activation = 'relu'),
+        Conv2D(32, (3, 3), input_shape = (50, 50, 3), activation = 'relu'),
         MaxPooling2D(pool_size = (2, 2)),
         
-        Conv2D(44, (3, 3), activation = 'relu'),
+        Conv2D(32, (3, 3), activation = 'relu'),
         MaxPooling2D(pool_size = (2, 2)),
         
-        Conv2D(88, (3, 3), activation = 'relu'),
+        Conv2D(64, (3, 3), activation = 'relu'),
         MaxPooling2D(pool_size = (2, 2)),
         
         Flatten(),
-        Dense(88, activation = 'relu'),
+
+        Dense(64, activation = 'relu'),
         Dropout(0.5),
+
         Dense(10, activation = 'sigmoid')
-
-        # Flatten(input_shape = (50, 50, 3)),
-        # Dense(256, activation = 'relu'),
-        # Dropout(0.5),
-        # Dense(128, activation = 'relu'),
-        # Dropout(0.5),
-        # Dense(10, activation = 'sigmoid')
-
-        #Dense(16, input_shape = (50, 50, 3), activation = 'relu', kernel_regularizer = regularizers.l1(0.01)),
-        #Conv2D(32, kernel_size = (1, 1), activation = 'tanh', padding = 'valid'),
-        #MaxPooling2D(pool_size = (3, 3)),
-        #Conv2D(32, kernel_size = (2, 2), activation = 'tanh', padding = 'valid'),
-        #MaxPooling2D(pool_size = (3, 3)),
-        #Flatten(),
-        #Dense(10, activation = 'relu')
     ])
     opt = keras.optimizers.Adam(learning_rate = 0.01)
     model.compile(
@@ -53,16 +40,6 @@ def defineModel():
         metrics = ['accuracy']
     )
     return model
-    # input1 = Dense(5, input_shape = (None, None, 3), activation = 'relu')
-    # conv1 = Conv2D(32, (3, 3), input_shape=(3, None, None), activation="relu")(inpu1)
-    # pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    # conv2 = Conv2D(32, (3, 3), activation="relu")(pool1)
-    # pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    # flat1 = Flatten()(pool2)
-    # dense1 = Dense(16, activation="relu")(flat1)
-    # dense2 = Dense(1, activation="sigmoid")(dense1)
-    # model = Model(inputs=input1, outputs=dense2)
-    # model.compile(loss='mse', optimizer='adadelta', metrics=['mse', 'mae'])
 
 # Checked matplotlib for how to create two-axis graphs:
 # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/two_scales.html
