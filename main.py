b5b91d61596f6ddb11b348d87bccbd0ff6e60f29
Sindre Eiklid <sindre0830@gmail.com>
2021-05-29 12:06:24 +0200

    Added cross validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import KFold, cross_val_score
 
 #get dataset
 
@@ -16,6 +17,22 @@ else:
     data = segmentData(images, masks)
     cacheDataset(data, labels)
 
+iterations = 50
+batchSize = 256
+
+#validate dataset
+
+model = tf.keras.wrappers.scikit_learn.KerasClassifier(
+    build_fn = defineModel, 
+    epochs = iterations, 
+    batch_size = batchSize, 
+    verbose = 0
+)
+
+kfold = KFold(n_splits = 5, shuffle = True, random_state = 0) 
+results = cross_val_score(model, data, labels, cv = kfold)
+print("Cross validation score: " + str(results))
+
 #train model
 
 X_train, X_test, y_train, y_test = prepareData(data, labels)
@@ -24,8 +41,8 @@ results = model.fit(
     X_train, 
     y_train, 
     validation_data = (X_test, y_test), 
-    batch_size = 256, 
-    epochs = 25,
+    batch_size = batchSize, 
+    epochs = iterations,
     verbose = 1
 )
 model.evaluate(X_test, y_test)
@@ -33,12 +50,11 @@ plot(results)
 
 #validate model
 
-Y_pred = model.predict(X_test)
-Y_pred = np.argmax(Y_pred, axis = 1)
-y_test = np.argmax(y_test, axis = 1)
-target_names = ["1","2","3","4","5","6","7","8","9","10"]
-print(confusion_matrix(y_test, Y_pred))
-print(classification_report(y_test, Y_pred, target_names = target_names, zero_division = 1))
+# Y_pred = model.predict(X_test)
+# Y_pred = np.argmax(Y_pred, axis = 1)
+# y_test = np.argmax(y_test, axis = 1)
+# target_names = ["1","2","3","4","5","6","7","8","9","10"]
+# print(confusion_matrix(y_test, Y_pred))
+# print(classification_report(y_test, Y_pred, target_names = target_names, zero_division = 1))
 
-tf.keras.utils.plot_model(model, show_shapes=True)
-
+#tf.keras.utils.plot_model(model, show_shapes = True)
